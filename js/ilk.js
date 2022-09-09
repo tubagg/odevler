313989b5e067a25df174edb1ac830f0d6421d206
Tuba Bozkurt <tubzkrt@gmail.com>
2022-09-09 21:38:21 +0300

    js1

---
+++ js/ilk.js
@@ -0,0 +1,33 @@
+var servername="tubatuba"
+console.log(servername);
+let fullname="Tugba bozkurt"
+console.log(fullname);
+const serverpassword="123456"
+console.log(serverpassword);
+//const-Sabit degişmicek seyler için kullanılır.Bir kere kullanılır.
+//let -değistirilebilir.(var ile benzer)
+let counter=320;
+counter=counter+1;//uzun yontem
+counter+=1;//yontem2
+counter++;//yontem1
+console.log(counter);
+console.log((10+5)*6);//işlem onceliği
+//Boolean
+let isActive=true//1
+isActive=false//0
+console.log(isActive);
+
+let username;
+let isusername=Boolean(username);
+console.log(isusername);
+
+username="user";
+console.log(Boolean(username));
+//boolean içinde veri varsa "true",yoksa "false"gözükür.
+//verilerin içi dolu mu dıye konrol etmek için kullanılır.
+
+
+Boolean (0==0);//trues
+//karar yapısı
+
+
